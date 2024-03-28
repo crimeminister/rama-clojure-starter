8f90906c1dbdbb7c528f5146fccde03ac88d25e9
Robert Medeiros <robert@crimeminister.org>
2024-03-28 16:38:58 -0400

    chore: update pstates.diff example

--- src/rama_clojure_starter/examples/pstates/diff.clj
+++ src/rama_clojure_starter/examples/pstates/diff.clj
@@ -6,22 +6,20 @@
   (:import
    [com.rpl.rama.diffs KeysDiff NewValueDiff UnknownDiff Diff Diff$Processor KeyDiff KeyDiff$Processor]))
 
-(defprotocol ProcessedKeys
-  (pkeys [this] "Return the set of processed keys."))
 
 (defn make-processor []
   (let [processed-keys (atom #{})]
     (reify
-      ProcessedKeys
-      (pkeys [this]
+      clojure.lang.IDeref
+      (deref [_]
         @processed-keys)
 
       Diff$Processor
-      (^void unhandled [this]
+      (^void unhandled [_]
         (swap! processed-keys conj :unknown))
 
       KeyDiff$Processor
-      (^void processKeyDiff [this ^KeyDiff diff]
+      (^void processKeyDiff [_ ^KeyDiff diff]
        (swap! processed-keys conj (.getKey diff))))))
 
 (defn process-keys-diff []
@@ -35,14 +33,14 @@
         diff (KeysDiff. m)
         processor (make-processor)]
     (.process diff processor)
-    (println "Processed keys 1:" (.pkeys processor)))
+    (println "Processed keys 1:" @processor))
 
   (let [processor (make-processor)
         ;; UnknownDiff; the change between old and new values is
         ;; unknown.
         diff (UnknownDiff.)]
     (.process diff processor)
-    (println "Processed keys 2:" (.pkeys processor))))
+    (println "Processed keys 2:" @processor)))
 
 (defn -main []
   (process-keys-diff))
